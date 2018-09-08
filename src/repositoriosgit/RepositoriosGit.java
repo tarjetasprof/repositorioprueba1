1b15ecf3e0ab8ddb64c535328fee636d2cd98727
guille <guille@cpe-80ee734c16c0.fibertel.com.ar>
2018-09-08 14:02:46 -0300

    Probando mi primer repositorio en github virtual.

--- src/repositoriosgit/RepositoriosGit.java
+++ src/repositoriosgit/RepositoriosGit.java
@@ -17,7 +17,7 @@ public class RepositoriosGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Prueba iniciando git");
-        
+        System.out.println("Prueba subiendo");
     }
     
 }
